822f79c340e460b0d084a9170847cf370e551edc
sergi.herrador.7e7@itb.cat <sergi.herrador.7e7@itb.cat>
2024-04-23 10:43:37 +0200

    FUNCIONANDO

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,10 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation(kotlin("test"))
+    implementation("org.mongodb:mongodb-driver-sync:4.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation ("org.apache.logging.log4j:log4j-slf4j-impl:2.17.1")
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,86 @@
 package org.example
 
+import com.mongodb.MongoException
+import com.mongodb.MongoTimeoutException
+import com.mongodb.client.MongoClient
+import com.mongodb.client.MongoClients
+import com.mongodb.client.MongoCollection
+import org.bson.Document
+import java.io.File
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    var mongoClient: MongoClient? = null
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    try {
+        // Connectar-se a servidor de mongoDB
+        val connectionString =
+            "mongodb+srv://elkin:pepoClown123@sergioherrador.bwwhoy4.mongodb.net/?retryWrites=true&w=majority&appName=SergioHerrador"
+        // Crear connexió
+        mongoClient = MongoClients.create(connectionString)
+        // Obtenir database SergioHerradorDiazLopez de la connexió a servidor
+        val db = mongoClient.getDatabase("itb")
+        // Obtenir col·lecció  grades de la database
+        val coll: MongoCollection<Document> = db.getCollection("productes")
+        val collRestaurants: MongoCollection<Document> = db.getCollection("restaurants")
+
+        exercici1(coll)
+        exercici2(collRestaurants)
+        // Control de errors.
+    } catch (e: MongoTimeoutException) {
+        println("No arribem a la base de dades")
+    } catch (e: MongoException) {
+        println(e.message)
+    } catch (e: Exception) {
+        println(e.message)
+    } finally {
+        // Cerrar la conexión de MongoDB al finalizar
+        mongoClient?.close()
     }
+}
+
+fun exercici1(coll: MongoCollection<Document>) {
+    // Lectura del archivo JSON
+    val jsonFile = File("src/main/JSON/products.json")
+    println(jsonFile.absolutePath)
+
+    var jsonDocuments: MutableList<String> = mutableListOf()
+    var nRegistros = 0
+    jsonFile.forEachLine {
+        jsonDocuments.add(it)
+
+        nRegistros++
+
+        if (nRegistros == 1000) {
+            coll.insertMany(jsonDocuments.map { Document.parse(it) })
+            nRegistros = 0
+            jsonDocuments = mutableListOf()
+        }
+    }
+    coll.insertMany(jsonDocuments.map { Document.parse(it) })
+
+
+}
+
+fun exercici2(coll: MongoCollection<Document>) {
+    // Lectura del archivo JSON
+    val jsonFile = File("src/main/JSON/restaurants.json")
+    println(jsonFile.absolutePath)
+
+    var jsonDocuments: MutableList<String> = mutableListOf()
+
+    var nRegistros = 0
+    jsonFile.forEachLine {
+        jsonDocuments.add(it)
+
+        nRegistros++
+
+        if (nRegistros == 1000) {
+            coll.insertMany(jsonDocuments.map { Document.parse(it) })
+            nRegistros = 0
+            jsonDocuments = mutableListOf()
+        }
+    }
+    coll.insertMany(jsonDocuments.map { Document.parse(it) })
 }
